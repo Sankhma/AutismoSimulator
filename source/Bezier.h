cfa14a9da157c18e3eb300fa4e7cde48b69884db
Sankhma <thedomiprince@gmail.com>
2020-11-07 13:51:40 +0000

    soem changes

--- source/Bezier.h
+++ source/Bezier.h
@@ -12,10 +12,16 @@ private:
     Vector2 point4;
     //TODO - dynamic number of points
 
+    LinkedList* root;
+     
+
 public:
     Bezier2(Vector2 point1, Vector2 point2);
     Bezier2(Vector2 point1, Vector2 point2, Vector2 point3);
     Bezier2(Vector2 point1, Vector2 point2, Vector2 point3, Vector2 point4);
+
+    // template<typename First, typename ... Vectors>
+    // Bezier2(First arg, const Vectors& ... rest);
 
     static Vector2 GenerateVertex(const Bezier2& bezier2, const double& t);
 };
