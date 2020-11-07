690e7d712519b27ac8c69b8bdeeb3cff13035a21
Sankhma <thedomiprince@gmail.com>
2020-11-07 13:20:27 +0000

    testing

--- source/Bezier.h
+++ source/Bezier.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vector.h"
+#include "Matrix.h"
 
 struct Bezier2{
 private:
---
+++ source/test.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+void Output() {
+    std::cout<<std::endl;
+}
+
+template<typename First, typename ... Strings>
+void Output(First arg, const Strings&... rest) {
+    std::cout<<arg<<" ";
+    Output(rest...);
+}
+
+int main() {
+    Output("I","am","a","sentence");
+    Output("Let's","try",1,"or",2,"digits");
+    return 0;
+}
