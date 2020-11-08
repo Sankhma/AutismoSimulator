2eb84fd9926949117dcccb641831f8b3e3c35e32
Sankhma <thedomiprince@gmail.com>
2020-11-08 21:45:32 +0000

    Generating vertexes works LMAO

--- source/Bezier.h
+++ source/Bezier.h
@@ -7,19 +7,11 @@
 struct Bezier2{
 private:
     unsigned int m_points = 0;
-    // Vector2 point1;
-    // Vector2 point2;
-    // Vector2 point3;
-    // Vector2 point4;
-    //TODO - dynamic number of points
     std::vector<Vector2> points;
 public:
     void addPoint(const Vector2& Point);
 
 public:
-    // Bezier2(Vector2 point1, Vector2 point2);
-    // Bezier2(Vector2 point1, Vector2 point2, Vector2 point3);
-    // Bezier2(Vector2 point1, Vector2 point2, Vector2 point3, Vector2 point4);
 
     Bezier2(const std::vector<Vector2>&);
     Bezier2(const int& size, ...);
--- source/bezier_debug.cpp
+++ source/bezier_debug.cpp
@@ -4,9 +4,9 @@
 #include "Vector.h"
 
 int main(){
-    Vector2 vec0 = Vector2(12, 13);
-    Vector2 vec1 = Vector2(123, 5);
-    Vector2 vec2 = Vector2(124, 234);
+    Vector2 vec0 = Vector2(1, 2);
+    Vector2 vec1 = Vector2(2, 3);
+    Vector2 vec2 = Vector2(7, 4);
     Vector2 vec3 = Vector2(133, 1123);
 
     std::vector<Vector2> points;
@@ -17,7 +17,7 @@ int main(){
 
     Bezier2 bez0 = Bezier2(points);
 
-    int steps = 20;
+    int steps = 5;
 
     std::cout << "Bezier2 has 4 points in total" << std::endl;
 
@@ -26,19 +26,27 @@ int main(){
         Bezier2::GenerateVertex(bez0, t);
     }
 
-    // Not working LMAO
+    bez0.addPoint(Vector2(1, 2));
+    bez0.addPoint(Vector2(420, 69));
 
-    // bez0.addPoint(Vector2(1, 2));
-    // bez0.addPoint(Vector2(420, 69));
-
-    // std::cout << "Added 2 more points, Bezier2 has 6 points in total" << std::endl;
-    // for(int i=0; i <= steps; i++){
-    //     double t = double(i) / steps;
-    //     Bezier2::GenerateVertex(bez0, t);
-    // }
+    std::cout << "Added 2 more points, Bezier2 has 6 points in total" << std::endl;
+    for(int i=0; i <= steps; i++){
+        double t = double(i) / steps;
+        Bezier2::GenerateVertex(bez0, t);
+    }
 
     Bezier2 bez1 = Bezier2(3, &vec0, &vec1, &vec2);
     std::cout << "Bezier2 (initialized using variadic arguments) has 3 points in total" << std::endl;
+
+    for(int i=0; i <= steps; i++){
+        double t = double(i) / steps;
+        Bezier2::GenerateVertex(bez1, t);
+    }
+
+    bez1.addPoint(Vector2(1, 2));
+    bez1.addPoint(Vector2(1, 10));
+
+    std::cout << "Added 2 more point, Bezier2 has 5 points in total" << std::endl;
 
     for(int i=0; i <= steps; i++){
         double t = double(i) / steps;
