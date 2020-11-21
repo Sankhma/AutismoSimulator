dad89cb1262071e9a348d23b58e1c3688914782e
Acors24 <acors@protonmail.com>
2020-11-21 16:25:54 +0100

    Bezier: Remove Bezier2 and Bezier3 and make templates (in cpp file) for Bezier #47

--- source/bezier_test.cpp
+++ source/bezier_test.cpp
@@ -4,35 +4,44 @@
 #include "Bezier.h"
 
 int main(){
-    std::cout << "-============ Bezier2 test =============-" << std::endl;
+    // std::cout << "-============ Bezier2 test =============-" << std::endl;
+	// {
+    //     int steps = 20;
+
+	// 	Vector2 p0 = {0, 0};
+    //     Vector2 p1 = {1, 1};
+    //     Vector2 p2 = {10, -10};
+    //     Vector2 p3 = {3.14, 2.73};
+
+    //     Bezier2 b0 = Bezier2(2, &p0, &p1);
+    //     Bezier2 b1 = Bezier2(3, &p0, &p1, &p2);
+    //     Bezier2 b2 = Bezier2(4, &p0, &p1, &p2, &p3);
+
+    //     std::cout << "Bezier2 for 2 points, steps=20" << std::endl;
+    //     for(int i=0; i <= steps; i++){
+    //         Vector2 temp = Bezier2::GenerateVertex(b0, i / double(steps));
+    //         std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
+    //     }
+
+    //     std::cout << "Bezier2 for 3 points, steps=20" << std::endl;
+    //     for(int i=0; i <= steps; i++){
+    //         Vector2 temp = Bezier2::GenerateVertex(b1, i / double(steps));
+    //         std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
+    //     }
+
+    //     std::cout << "Bezier2 for 4 points, steps=20" << std::endl;
+    //     for(int i=0; i <= steps; i++){
+    //         Vector2 temp = Bezier2::GenerateVertex(b2, i / double(steps));
+    //         std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
+    //     }
+	// }
+
+    std::cout << "-============ Bezier template test =============-" << std::endl;
 	{
-        int steps = 20;
-
-		Vector2 p0 = {0, 0};
-        Vector2 p1 = {1, 1};
-        Vector2 p2 = {10, -10};
-        Vector2 p3 = {3.14, 2.73};
-
-        Bezier2 b0 = Bezier2(2, &p0, &p1);
-        Bezier2 b1 = Bezier2(3, &p0, &p1, &p2);
-        Bezier2 b2 = Bezier2(4, &p0, &p1, &p2, &p3);
-
-        std::cout << "Bezier2 for 2 points, steps=20" << std::endl;
-        for(int i=0; i <= steps; i++){
-            Vector2 temp = Bezier2::GenerateVertex(b0, i / double(steps));
-            std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
-        }
-
-        std::cout << "Bezier2 for 3 points, steps=20" << std::endl;
-        for(int i=0; i <= steps; i++){
-            Vector2 temp = Bezier2::GenerateVertex(b1, i / double(steps));
-            std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
-        }
-
-        std::cout << "Bezier2 for 4 points, steps=20" << std::endl;
-        for(int i=0; i <= steps; i++){
-            Vector2 temp = Bezier2::GenerateVertex(b2, i / double(steps));
-            std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
-        }
+		int steps = 10;
+		Bezier<Vector3> b1 = Bezier<Vector3>({{0, 0}, {1, 3}, {4, 3}, {3, 0}});
+		for (int i = 0; i <= steps; i++) {
+			Bezier<Vector3>::GenerateVertex(b1, (double)i / steps);
+		}
 	}
 }
