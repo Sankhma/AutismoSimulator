69321eae83f377340b76d14ad2ee74aa1bba77f9
Sankhma <thedomiprince@gmail.com>
2020-11-21 17:20:55 +0000

    GitHub is laughing @ me

--- source/bezier_test.cpp
+++ source/bezier_test.cpp
@@ -10,10 +10,10 @@ int main(){
 	{
         int steps = 20;
 
-		Vector2 p0 = {0, 0};
-        Vector2 p1 = {1, 1};
-        Vector2 p2 = {10, -10};
-        Vector2 p3 = {3.14, 2.73};
+		Vector2<double> p0 = {0, 0};
+        Vector2<double> p1 = {1, 1};
+        Vector2<double> p2 = {10, -10};
+        Vector2<double> p3 = {3.14, 2.73};
 
         Bezier2 b0 = Bezier2(2, &p0, &p1);
         Bezier2 b1 = Bezier2(3, &p0, &p1, &p2);
@@ -21,19 +21,19 @@ int main(){
 
         std::cout << "Bezier2 for 2 points, steps=20" << std::endl;
         for(int i=0; i <= steps; i++){
-            Vector2 temp = Bezier2::GenerateVertex(b0, i / double(steps));
+            Vector2<double> temp = Bezier2::GenerateVertex(b0, i / double(steps));
             std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
         }
 
         std::cout << "Bezier2 for 3 points, steps=20" << std::endl;
         for(int i=0; i <= steps; i++){
-            Vector2 temp = Bezier2::GenerateVertex(b1, i / double(steps));
+            Vector2<double> temp = Bezier2::GenerateVertex(b1, i / double(steps));
             std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
         }
 
         std::cout << "Bezier2 for 4 points, steps=20" << std::endl;
         for(int i=0; i <= steps; i++){
-            Vector2 temp = Bezier2::GenerateVertex(b2, i / double(steps));
+            Vector2<double> temp = Bezier2::GenerateVertex(b2, i / double(steps));
             std::cout << "[ " << temp.x << ", " << temp.y << " ]" << std::endl;
         }
 	}
