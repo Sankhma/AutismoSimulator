797ad40be9e34f20844d37486978d672bf5c90e3
Acors24 <acors@protonmail.com>
2020-11-25 20:56:30 +0100

    Added a destructor for LinkedList and renamed addNode() method to add().

--- source/linked_test.cpp
+++ source/linked_test.cpp
@@ -1,33 +1,12 @@
 #include <iostream>
 #include "Matrix.h"
 
-void displayList(const LinkedList &list) {
-	Node *current = list.head;
-	while (current != nullptr) {
-		std::cout << current->value << ", ";
-		current = current->next;
-	}
-	std::cout << std::endl;
-}
-
 int main() {
-	LinkedList list1 = LinkedList();
-	for (int i = 0; i < 10; i++) {
-		list1.addNode(i);
+	auto list = LinkedList<int>();
+
+	for (int i = 0; i < 5; i++) {
+		list.add(i);
 	}
-	
-	std::cout << "list1: ";
-	displayList(list1);
-	
-	LinkedList list2 = list1.copy();
-
-	list1[3] = 69;
-
-	std::cout << "list1: ";
-	displayList(list1);
-	std::cout << "list2: ";
-	displayList(list2);
-
 
 	return 0;
 }
