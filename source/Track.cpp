10ea4ecfe9ab9b90bb49eeac5acaa9c788ce02f9
Dominik Paul <thedomiprince@gmail.com>
2020-12-19 17:59:28 +0000

    Making some progress on generateTrack function

--- source/Track.cpp
+++ source/Track.cpp
@@ -35,15 +35,66 @@ template<typename T>
 Track<T> Track<T>::GenerateTrack(std::string &type) {
     std::cout << "Generating Track: " << std::endl;
     if(type == "Vector2") {
+        char bezierAnswer = 'y';
+        char vectorAnswer = 'y';
+        bool valid;
         double x, y;
         std::string temp;
-        Bezier<Vector2<double>> bezier;
+        std::vector<Bezier<Vector2<double>>> *bezierList;
+        Bezier<Vector2<double>> *bezier;
+        Vector2<double> *vector;
 
-        std::cout << "\tstarting at the origin (0, 0)" << std::endl;;
-        std::cout << "\tspecify the next point: ";
-        std::cin >> x, y;
+        std::cout << "\tstarting at the origin (0, 0)" << std::endl;
 
-        
+        vector->x = 0;
+        vector->y = 0;
+
+        bezier->addPoint(vector);
+
+        while(bezierAnswer == 'y') {
+            vector->x = (bezier->points.head)->value.x;
+            vector->y = (bezier->points.head)->value.y;
+
+            bezier->clear();
+
+            bezier->addPoint(vector);
+
+            while(vectorAnswer == 'y') {
+                std::cout << "\tspecify the next point: \n";
+                do{
+                    std::cout << "\t\tx: ";
+                    std::cin >> x;
+                    std::cout << "\t\ty: ";
+                    std::cin >> y;
+
+                    vector->x = x;
+                    vector->y = y;
+
+                    if(bezier->m_points == 0) {
+                        //TODO: check if the point is valid.
+                        
+                        std::cout << "Invalid point. The point should lie on the line going through the penultimate and last points in last Bezier curve, to ensure continuity C1.\n";
+                    } else {
+                        valid = true;
+                    }
+                } while(valid);
+                
+
+                bezier->addPoint(vector);
+
+                std::cout << "Points in this bezier: " << bezier->m_points << std::endl;
+
+                std::cout << "\n\tDo you wish to continue adding points to this bezier? (y/n): ";
+                std::cin >> vectorAnswer;
+            }
+
+            bezierList->push_back(bezier);
+            std::cout << "Number of Bezier curves: " << bezierList->size() << std::endl;
+
+
+            std::cout << "\n\tDo you wish to continue? (y/n): ";
+            std::cin >> bezierAnswer;
+        }
     }
 }
 
